aba8e32bdcd670281afb4c76335c9b2bc26eda2a
Benjamin-Endale <onyxbngame@gmail.com>
2025-03-23 21:19:30 +0300

    Pup

--- Question_8/index.js
+++ Question_8/index.js
@@ -4,13 +4,17 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://monkeytype.com/");
-  -(await page.evaluate(() => {
-    const words = [];
-    const word_active = document.querySelectorAll(".word");
-    let actualWord = word_active.document.querySelectorAll("letter").innerText;
-    // actualWord.forEach((letter) => {
-    //   words.push[letter];
-    // });
-    console.log(actualWord);
-  }));
+  const grabpara = await page.evaluate(() => {
+    const atags = [];
+    const pgTag = document.querySelectorAll(".word");
+    pgTag.forEach((element) => {
+      atags.push(element.innerText);
+    });
+    for (i = 0; i < atags.length; i++) {
+      page.type(".pgTag", `${atags[i]}`);
+    }
+    return atags;
+  });
+  console.log(grabpara);
+  await browser.close();
 })();

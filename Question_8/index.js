b22bf312e44f571648a40d9d8529dcf363c86c57
Benjamin-Endale <onyxbngame@gmail.com>
2025-03-23 21:53:38 +0300

    Puppeteer web crawling

--- Question_8/index.js
+++ Question_8/index.js
@@ -3,18 +3,31 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://monkeytype.com/");
-  const grabpara = await page.evaluate(() => {
-    const atags = [];
-    const pgTag = document.querySelectorAll(".word");
-    pgTag.forEach((element) => {
-      atags.push(element.innerText);
-    });
-    for (i = 0; i < atags.length; i++) {
-      page.type(".pgTag", `${atags[i]}`);
+  await page.goto(
+    "https://benjamin-endale.github.io/Special.Topics/Question_6/",
+    {
+      waitUntil: "domcontentloaded",
+      timeout: 30000, // Set a timeout of 30 seconds
     }
-    return atags;
+  );
+
+  const myPage = await page.evaluate(() => {
+    const titlepage = document.querySelector("title").innerText;
+    const a = document.querySelectorAll(".aTag");
+    let hrefCollection = [];
+
+    a.forEach((anchor) => {
+      hrefCollection.push(anchor.href);
+    });
+
+    return {
+      title: titlepage,
+      hrefs: [...new Set(hrefCollection)],
+    };
   });
-  console.log(grabpara);
+
+  console.log("Website Title:", myPage.title);
+  console.log("Unique Href Collection:", myPage.hrefs);
+
   await browser.close();
 })();
